9ba4fdb22543eaa76fbac24aea6f111ef02d877f
Vitor Spiri <vitorspiri1@gmail.com>
2025-11-23 10:01:51 -0300

    feat: "initial project and author class"

---
+++ LojaLivrosVirtual/LojaLivrosVirtual/Entities/Author.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LojaLivrosVirtual.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool DeletedDate { get; set; }
+
+        public Author(string name, string email, string description)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name cannot be null or empty.", nameof(name));
+            }
+
+            if (!IsValidEmailWithRegex(email))
+            {
+                throw new ArgumentException("Email has to be valid.", nameof(name));
+            }
+
+            if (string.IsNullOrWhiteSpace(description))
+            {
+                throw new ArgumentException("Description cannot be null or empty.", nameof(name));
+            }
+
+            if (description.Length > 400)
+            {
+                throw new ArgumentException("Description cannot have more then 400 caracteres", nameof(name));
+            }
+
+            Name = name;
+            Email = email;
+            Description = description;
+        }
+
+        public static bool IsValidEmailWithRegex(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return false;
+            }
+
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, pattern);
+        }
+    }
+}
