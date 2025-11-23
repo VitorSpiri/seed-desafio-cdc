bb788e9cd687412604707194472a8cccc639ec1a
Vitor Spiri <vitorspiri1@gmail.com>
2025-11-23 20:58:02 -0300

    feat: new author simple method

---
+++ LojaLivrosVirtual/LojaLivrosVirtual/Controllers/AuthorController.cs
@@ -0,0 +1,24 @@
+﻿using LojaLivrosVirtual.DTOs.Author;
+using LojaLivrosVirtual.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaLivrosVirtual.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthorController : ControllerBase
+{
+    // GET: api/<AuthorController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] AuthorRequest request)
+    {
+        var author = new Author(request.Name, request.Email, request.Description);
+        return Ok(author);
+    }
+}
---
+++ LojaLivrosVirtual/LojaLivrosVirtual/DTOs/Author/AuthorRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace LojaLivrosVirtual.DTOs.Author
+{
+    public class AuthorRequest
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+    }
+}
